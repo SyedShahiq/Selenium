95f9dd1ecbf66c73f9f69d90881c15f5bb1e0a69
Syed Shahiq <shahiqurrehman@gmail.com>
2019-12-12 17:29:06 +0500

    Assignment 1

---
+++ assignment_1.py
@@ -0,0 +1,24 @@
+import unittest
+import time
+
+from selenium import webdriver
+
+class Login(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+
+    def test_login(self):
+        self.driver.get('https://awesomeblogposts.herokuapp.com/login/')
+        username = self.driver.find_element_by_css_selector('input[id="id_username"]')
+        username.send_keys('shahiq')
+        password = self.driver.find_element_by_css_selector('input[id="id_password"]')
+        password.send_keys('shahiq123')
+        button = self.driver.find_element_by_css_selector('button[type="submit"]')
+        button.click()
+        time.sleep(2)
+        assert 'https://awesomeblogposts.herokuapp.com/' == self.driver.current_url
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
