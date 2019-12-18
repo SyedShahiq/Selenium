17505553a4982e36e6a05059e6fc9388cd684c27
Syed Shahiq <shahiqurrehman@gmail.com>
2019-12-18 17:11:57 +0500

    Dashboard page added

---
+++ Assignment_2/pages/dashboard.py
@@ -0,0 +1,12 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+class DashboardPage(object):
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(self.driver,15)
+
+    def check_dashboard_title(self):
+        self.wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR,'title'),'Dashboard'))
+
